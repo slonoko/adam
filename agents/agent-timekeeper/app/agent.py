eb29e71b79c91c1fd4fb166d354785d4272bae8a
Elie Khoury <elie.kh@gmail.com>
2025-08-17 11:23:03 +0200

    still not working

--- agents/agent-timekeeper/app/agent.py
+++ agents/agent-timekeeper/app/agent.py
@@ -31,33 +31,20 @@ from google.adk.auth.auth_credential import (
     AuthCredentialTypes,
     HttpAuth,
     HttpCredentials,
-    AuthCredential,
+    AuthCredential,ServiceAccount, ServiceAccountCredential
 )
+from google.adk.tools.openapi_tool.auth.auth_helpers import service_account_scheme_credential
+import json
+from google.auth import crypt, jwt
+import time
+from google.auth import compute_engine
 
-_, project_id = google.auth.default()
-os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
-os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "us-central1")
-os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
+#_, project_id = google.auth.default()
+# os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
+# os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "us-central1")
+# os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
 load_dotenv()
-
-
-def get_identity_token():
-    credentials, _ = google_auth.default()
-    auth_request = google_requests.Request()
-    credentials.refresh(auth_request)
-    return credentials.token
-
-
-def custom_tool_filter(tool, readonly_context=None):
-    tool_name = tool.name if hasattr(tool, "name") else str(tool)
-    logging.info(f"Looking for {tool}")
-    return tool_name.startswith("t_")
-
-
-logging.getLogger("google_adk.google.adk.tools.base_authenticated_tool").setLevel(
-    logging.ERROR
-)
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -66,13 +53,70 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-auth_cred = AuthCredential(
-    auth_type=AuthCredentialTypes.HTTP,
-    http=HttpAuth(
-        scheme="bearer",
-        credentials=HttpCredentials(token=get_identity_token()),
-    ),
-)
+def get_sa_token():
+    credentials, _ = google_auth.load_credentials_from_file("/home/elie/Projects/adam/agents/agent-timekeeper/adam-sa.json",scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    auth_request = google_requests.Request()
+    credentials.refresh(auth_request)
+    return credentials.token
+
+def get_identity_token():
+  credentials, _ = google_auth.default()
+  auth_request = google_requests.Request()
+  credentials.refresh(auth_request)
+  
+  return credentials.token
+
+def custom_tool_filter(tool, readonly_context=None):
+    tool_name = tool.name if hasattr(tool, "name") else str(tool)
+    logging.info(f"Looking for {tool}")
+    return tool_name.startswith("t_")
+
+def generate_jwt(
+    sa_keyfile = "/home/elie/Projects/adam/agents/agent-timekeeper/adam-sa.json",
+    sa_email="adam-agent@assistant-424508.iam.gserviceaccount.com",
+    audience="mcp-server-186919647719.us-central1.run.app",
+    expiry_length=3600,
+):
+    """Generates a signed JSON Web Token using a Google API Service Account."""
+
+    now = int(time.time())
+
+    # build payload
+    payload = {
+        "iat": now,
+        # expires after 'expiry_length' seconds.
+        "exp": now + expiry_length,
+        # iss must match 'issuer' in the security configuration in your
+        # swagger spec (e.g. service account email). It can be any string.
+        "iss": "https://accounts.google.com",
+        # aud must be either your Endpoints service name, or match the value
+        # specified as the 'x-google-audience' in the OpenAPI document.
+        "aud": audience,
+        # sub and email should match the service account's email address
+        "sub": sa_email,
+    }
+
+    # sign with keyfile
+    signer = crypt.RSASigner.from_service_account_file(sa_keyfile)
+    output = jwt.encode(signer, payload)
+
+    return output
+
+with open("/home/elie/Projects/adam/agents/agent-timekeeper/adam-sa.json") as f:
+    d = json.load(f)
+    sa_cred = ServiceAccountCredential.model_validate_json(json.dumps(d))
+
+sa = ServiceAccount(service_account_credential=sa_cred, scopes=["https://www.googleapis.com/auth/cloud-platform"])
+auth_scheme, auth_cred = service_account_scheme_credential(sa)
+
+# auth_cred = AuthCredential(
+#     auth_type=AuthCredentialTypes.SERVICE_ACCOUNT,
+#     service_account=ServiceAccount(service_account_credential=sa_cred, scopes=["https://www.googleapis.com/auth/cloud-platform"], use_default_credential=True), 
+#     # http=HttpAuth(
+#     #     scheme="bearer",
+#     #     credentials=HttpCredentials(token=get_identity_token()),
+#     # ),
+# )
 
 root_agent = LlmAgent(
     name="Timekeeper",
@@ -88,5 +132,5 @@ root_agent = LlmAgent(
         "and help manage schedules and reminders. "
         "and use the tool if certain words are mentioned in the user input, such as today, tomorrow, yesterday, now, current time, current date, schedule, reminder, etc. "
     ),
-    tools=[MCPToolset(auth_credential=auth_cred, errlog=None, connection_params=StreamableHTTPConnectionParams(url=f"{os.getenv('mcp_server_url')}/mcp"), tool_filter=custom_tool_filter)],  # type: ignore
+    tools=[MCPToolset(auth_scheme=auth_scheme, auth_credential=auth_cred, errlog=sys.stderr, connection_params=StreamableHTTPConnectionParams(url=f"{os.getenv('mcp_server_url')}/mcp"), tool_filter=custom_tool_filter)],  # type: ignore
 )
