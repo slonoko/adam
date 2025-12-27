56eca24607e211981c8c0217f885d67237bacfb3
Elie Khoury <elie.kh@gmail.com>
2025-12-27 11:16:39 +0200

    working a2a setup

--- adk/tradingadvisor/agent.py
+++ adk/tradingadvisor/agent.py
@@ -1,4 +1,3 @@
-import asyncio
 from google.adk.agents import LlmAgent, Agent
 import logging
 from dotenv import load_dotenv
@@ -8,7 +7,7 @@ from google.adk.planners import PlanReActPlanner
 import os
 from google.adk.models.lite_llm import LiteLlm
 import warnings
-import instructions
+from tradingadvisor import instructions
 from google.adk.models.lite_llm import LiteLlm
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
@@ -17,11 +16,6 @@ from google.adk.tools import load_web_page
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
 from google.adk.tools.agent_tool import AgentTool
 from google.adk.code_executors.container_code_executor import ContainerCodeExecutor
-from google.adk.sessions import InMemorySessionService, Session
-from google.adk.memory import InMemoryMemoryService # Import MemoryService
-from google.adk.runners import Runner
-from google.genai.types import Content, Part
-import uuid
 
 load_dotenv()
 
@@ -38,10 +32,6 @@ logger = logging.getLogger(__name__)
 
 # Suppress Pydantic validation for arbitrary types used internally
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-# Services must be shared across runners to share state and memory
-session_service = InMemorySessionService()
-memory_service = InMemoryMemoryService() # Use in-memory for demo
 
 def get_model():
     """Helper function to get the appropriate model based on MODEL_NAME environment variable."""
@@ -117,20 +107,4 @@ root_agent = Agent(
     tools=[load_memory,AgentTool(agent=broker_agent), AgentTool(agent=weather_agent), AgentTool(agent=stock_agent), AgentTool(agent=time_agent), AgentTool(agent=news_agent), AgentTool(agent=home_sensor_agent) ],
 )
 
-async def run():
-    runner = Runner(agent=root_agent,
-                    app_name="TradingAdvisor",
-                    session_service=session_service,
-                    memory_service=memory_service)
-    session_id = str(uuid.uuid4())
-    await runner.session_service.create_session(app_name="TradingAdvisor", user_id="elie", session_id=session_id)
-    user_input = Content(parts=[Part(text="What is the current value of nvidia in euros?")])
-    async for event in runner.run_async(user_id="elie", session_id=session_id, new_message=user_input):
-        if event.is_final_response() and event.content and event.content.parts:
-            final_response_text = event.content.parts[0].text
-    print(f"Agent 1 Response: {final_response_text}")
-    completed_session = await runner.session_service.get_session(app_name="TradingAdvisor", user_id="elie", session_id=session_id)
-    await memory_service.add_session_to_memory(completed_session)
-
-asyncio.run(run())
-# a2a_app = to_a2a(root_agent)
+a2a_app = to_a2a(root_agent)
--- client/agent/agent.py
+++ client/agent/agent.py
@@ -1,8 +1,59 @@
+from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
+from google.adk.runners import Runner
+from google.genai.types import Content, Part
+import uuid
+from google.adk.sessions.sqlite_session_service import SqliteSessionService
+from google.adk.memory import InMemoryMemoryService
+import asyncio
+import logging
+from dotenv import load_dotenv
+import sys
+from google.adk.tools.load_memory_tool import load_memory  # Tool to query memory
+import os
+import warnings
+
+load_dotenv()
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.getLogger("google_adk.google.adk.tools.base_authenticated_tool").setLevel(logging.ERROR)
+
+logging.debug("ClocknStock agent initialized with model: %s", os.getenv("MODEL_NAME", ""))
+logger = logging.getLogger(__name__)
+
+# Suppress Pydantic validation for arbitrary types used internally
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Services must be shared across runners to share state and memory
+session_service = SqliteSessionService("session.db")
+memory_service = InMemoryMemoryService() # Use in-memory for demo
 
 root_agent = RemoteA2aAgent(
-    model='gemini-2.5-flash',
-    name='root_agent',
+    name='trading_advisor',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    agent_card=(
+        f"http://localhost:8000/{AGENT_CARD_WELL_KNOWN_PATH}"
+    ),
 )
+
+async def run():
+    runner = Runner(agent=root_agent,
+                    app_name="TradingAdvisor",
+                    session_service=session_service,
+                    memory_service=memory_service)
+    session_id = str(uuid.uuid4())
+    await runner.session_service.create_session(app_name="TradingAdvisor", user_id="elie", session_id=session_id)
+    user_input = Content(parts=[Part(text="What is the current value of nvidia in euros?")])
+    async for event in runner.run_async(user_id="elie", session_id=session_id, new_message=user_input):
+        if event.is_final_response() and event.content and event.content.parts:
+            final_response_text = event.content.parts[0].text
+    print(f"Agent 1 Response: {final_response_text}")
+    completed_session = await runner.session_service.get_session(app_name="TradingAdvisor", user_id="elie", session_id=session_id)
+    await memory_service.add_session_to_memory(completed_session)
+
+# asyncio.run(run())
